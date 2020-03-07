d313043688f94344c6547045e5df4cf6f88be4cf
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-03-07 11:44:25 +0900

    memoListScreen作成

--- memol-app/App.js
+++ memol-app/App.js
@@ -1,19 +1,103 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.appbar}>
+        <Text style={styles.appbarTitle}>Memol</Text>
+      </View>
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2020-3-12</Text>
+        </View>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2020-3-12</Text>
+        </View>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2020-3-12</Text>
+        </View>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2020-3-12</Text>
+        </View>
+      </View>
+      <View style={styles.memoAddButton}>
+        <Text style={styles.memoAddButtonText}>+</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#FFFDF6',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  memoList: {
+    width: '100%',
+    flex: 1,
+    top: 78
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff'
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2'
+  },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 78,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.9,
+    shadowRadius: 3,
+    zIndex: 10
+  },
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.7,
+    shadowRadius: 3,
+    zIndex: 10
+  },
+  memoAddButtonText: {
+    fontSize: 24,
+    color: '#fff'
+  }
+})
+
+export default App
